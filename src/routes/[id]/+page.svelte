c15e199ff73d114e65f9c8292f604ef4ecc49979
devnull03 <56480041+devnull03@users.noreply.github.com>
2024-01-30 14:49:35 -0800

    feat: page rendering

---
+++ src/routes/[id]/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import site_contents from "$lib/utils/contents";
+  import { page } from "$app/stores";
+
+  const findContent = (id: string) => {
+    for (let i = 0; i < site_contents.length; i++) {
+      if (site_contents[i].id === id) {
+        return site_contents[i].content;
+      }
+
+      let chapters = site_contents[i].chapters;
+      if (chapters) {
+        for (let j = 0; j < chapters.length; j++) {
+          if (chapters[j].id === id) {
+            return chapters[j].content;
+          }
+        }
+      }
+    }
+  };
+
+  $: content = findContent($page.params.id);
+</script>
+
+
+
