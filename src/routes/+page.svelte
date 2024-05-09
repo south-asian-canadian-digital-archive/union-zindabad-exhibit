c9c9b3de04d360d1fd0d8da478bc5a504ff3fc9e
devnull03 <56480041+devnull03@users.noreply.github.com>
2024-05-09 18:36:00 +0530

    feaet: new home page design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,45 +34,38 @@
 </script>
 
 <div
-  class="bg-[url(/cover_bg.jpg)] p-6 md:p-20 lg:p-20 bg-cover h-full selection:bg-black selection:bg-opacity-50 selection:text-white"
+  class="bg-[url(/images/cover_img_0.jpg)] bg-cover h-full selection:bg-black selection:bg-opacity-50 selection:text-white"
 >
   <div
-    class="flex flex-col lg:flex-row md:flex-row overflow-clip justify-center gap-16"
+    class="bg-cover h-screen w-full p-8 flex flex-col justify-end text-white bg-[#5c5c5c] bg-opacity-20"
   >
-    <div class="flex flex-col gap-10 pt-10">
-      <div class="text-7xl z-10 text-shadow font-lexend-peta text-white pt-10">
-        <p>Union</p>
-        <p class="">Zindabad!</p>
-        <p class="text-sm pt-4">South Asian Canadian Labour History in British Columbia</p>
+    <!-- /images/cover_img_0.jpg -->
+    <div class="flex flex-col items-center gap-4 pt-10">
+      <div class="text-8xl z-10 font-mono pt-10 font-bold">
+        <p>UnionZindabad!</p>
+        <p class="text-base text-center">
+          South Asian Canadian Labour History in British Columbia
+        </p>
       </div>
 
-      <div class="lg:hidden md:hidden w-[80vw] -my-4 self-end">
-        <img src="/cover_right.png" alt="" class="" />
-      </div>
-
-      <div class="w-full flex justify-evenly pt-4">
-        <div class="bg-black rounded-lg w-fit z-10">
+      <div class="flex gap-4">
+        <button
+          class="rounded-lg p-2 px-6 font-bold bg-white border border-black text-[#000000D1] -translate-y-1 translate-x-1 hover:-translate-y-2 hover:translate-x-2 transition-all duration-500 ease-in-out"
+          on:click={() => {
+            goto("/south-asian-canadian-labour-history-project");
+          }}>Read Book</button
+        >
+        <a href="https://sacda.ca/index.php/Detail/collections/746">
           <button
-            class="rounded-lg p-2 px-6 md:text-lg lg:text-lg font-bold bg-white border border-black text-[#000000D1] -translate-y-1 translate-x-1 hover:-translate-y-2 hover:translate-x-2 transition-all duration-500 ease-in-out"
-            on:click={() => {
-              goto("/south-asian-canadian-labour-history-project");
-            }}>Read Book</button
+            class="rounded-lg p-2 px-6 font-bold bg-white border border-black text-[#000000D1] -translate-y-1 translate-x-1 hover:-translate-y-2 hover:translate-x-2 transition-all duration-500 ease-in-out"
           >
-        </div>
-
-        <div class="bg-black rounded-lg w-fit z-10">
-          <a href="https://sacda.ca/index.php/Detail/collections/746">
-            <button
-              class="rounded-lg p-2 px-6 md:text-lg lg:text-lg font-bold bg-white border border-black text-[#000000D1] -translate-y-1 translate-x-1 hover:-translate-y-2 hover:translate-x-2 transition-all duration-500 ease-in-out"
-            >
             View the Collection
-            </button>
-          </a>
-        </div>
+          </button>
+        </a>
       </div>
 
       <div
-        class="lg:text-base md:text-base mt-8 lg:mt-0 md:mt-0 text-xs text-left text-white md:max-w-[25vw] lg:max-w-[25vw] flex flex-col gap-4"
+        class="lg:text-base md:text-base mt-8 lg:mt-0 md:mt-0 text-xs text-center max-w-[70vw] flex flex-col gap-4"
       >
         Union Zindabad traces South Asian labor history in Canada through a
         variety of lenses. Beginning with the migrations of the early twentieth
@@ -81,36 +74,6 @@
       </div>
     </div>
 
-    <div class="h-full max-w-[50vw]">
-      <div
-        class="relative h-fit hidden lg:block md:block rounded-[20px] object-cover"
-      >
-        <img src="/cover_right.png" alt="" class="w-full" />
-        <p
-          hidden
-          class="absolute text-[#f5f5f5] text-right text-xs font-bold bottom-4 right-4 max-w-[70%]"
-        >
-          On March 20, 1943 District union official Nigel Morgan reported that
-          the 'East Indians' at the Youbou Mill were 100% unionized. Kaatza
-          Station Museum & Archives, N01603, IWA Local 1-80, Wilmer Gold Photo
-          Collection, ©United Steelworkers Local 1-1937.
-        </p>
-      </div>
-
-      <div
-        class="flex w-full justify-evenly lg:pt-14 md:pt-14 py-8 items-center h-full"
-      >
-        {#each logos as logo}
-          <a href={logo.link} target="_blank">
-            <img
-              src={logo.image}
-              alt={logo.name}
-              class="lg:w-[10vw] md:w-[10vw] w-[25vw] h-auto object-contain"
-            />
-          </a>
-        {/each}
-      </div>
-    </div>
   </div>
 </div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ export default {
     extend: {
       fontFamily: {
         'lexend-peta': ['Lexend Peta', 'sans-serif'],
+        'inter': ['Inter', 'sans-serif'],
       },
     },
   },
